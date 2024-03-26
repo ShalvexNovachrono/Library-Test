2549a3475ee13d3c0c853531eb54614ae4e0e01e
Shalvex Novachrono <55297590+ShalvexNovachrono@users.noreply.github.com>
2024-03-26 00:43:55 +0000

    Code added

---
+++ src/FrontEndSide/GUI_WORKER.java
@@ -0,0 +1,212 @@
+package FrontEndSide;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+class Front_End_Element_Code {
+
+    static Color FrameBackGroundColour = new Color(255, 255, 255);
+    static Color ButtonBackGroundColour = new Color(5, 74, 140);
+    static Color ButtonForeGroundColour = new Color(255, 255, 255);
+    static String FrameDefaultFontName = "Merriweather";
+
+
+    static Path CurrentRelativePath = Paths.get("");
+    static String CurrentRelativePathString = CurrentRelativePath.toAbsolutePath().toString();
+    static String PathDestinationToAssetsFolder = CurrentRelativePathString + "\\src\\Assets\\";
+    public static JFrame Create_Frame(String name, String LogoDestination, int width, int height) {
+        JFrame frame = new JFrame(name);
+        frame.getContentPane().setBackground(FrameBackGroundColour);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        ImageIcon logo = new ImageIcon(LogoDestination);
+        frame.setIconImage(logo.getImage());
+        frame.setVisible(true);
+        frame.setLayout(null);
+        return frame;
+    }
+
+    public static JPanel Create_Panel(int x, int y, int Width, int Height) {
+        JPanel Temp_Panel = new JPanel();
+        Temp_Panel.setBounds(x, y, Width, Height);
+        Temp_Panel.setBackground(FrameBackGroundColour);
+        Temp_Panel.setLayout(null);
+        return Temp_Panel;
+    }
+
+    public static JButton Create_Button(int x, int y, String text, int Width, int Height, int borderRadius) {
+        JButton temp_button = new RoundedButton(borderRadius, text);
+        temp_button.setFont(new java.awt.Font(FrameDefaultFontName, Font.BOLD, 14));
+        temp_button.setBackground(ButtonBackGroundColour);
+        temp_button.setForeground(ButtonForeGroundColour);
+        temp_button.setBounds(x, y, Width, Height);
+        temp_button.setContentAreaFilled(false);
+        temp_button.setBorderPainted(false);
+        return temp_button;
+    }
+
+    public static JTextField Create_TextField(int x, int y, int column, int Width, int Height) {
+        JTextField temp_input = new JTextField(column);
+        temp_input.setFont(new java.awt.Font(FrameDefaultFontName, Font.PLAIN, 14));
+        if ((Width != 0 && Height != 0) && (column == 0))
+            temp_input.setBounds(x, y, Width, Height );
+        return temp_input;
+    }
+
+    public static JLabel Create_Label(int x, int y, String text, int Width, int Height) {
+        JLabel temp_label = new JLabel(text);
+        temp_label.setForeground(Color.darkGray);
+        temp_label.setFont(new java.awt.Font(FrameDefaultFontName, Font.PLAIN, 11));
+        if (Width != 0 && Height != 0)
+            temp_label.setBounds(x, y, Width, Height );
+        return temp_label;
+    }
+
+    public static JList Create_List_Container(int x, int y, DefaultListModel List_Items, int Width, int Height) {
+        JList Temp_List = new JList(List_Items);
+        Temp_List.setBounds(x, y, Width, Height);
+        return Temp_List;
+    }
+
+    public static JScrollPane Create_ScrollPane(int x, int y, int Width, int Height) {
+        JScrollPane Temp_ScrollPane = new JScrollPane();
+        Temp_ScrollPane.setBounds(x, y, Width, Height);
+        return Temp_ScrollPane;
+    }
+
+    public static ImageIcon Create_Image_Icon(String Image_Destination) {
+        ImageIcon Temp_ImageIcon = new ImageIcon(Image_Destination);
+        return Temp_ImageIcon;
+    }
+    public static JLabel Create_Label_With_ImageIcon(int x, int y, String Image_Destination, int Width, int Height, Boolean resizeImage) {
+        ImageIcon image = Create_Image_Icon(Image_Destination);
+        JLabel temp_label;
+        if (resizeImage)
+            temp_label = new JLabel(new ImageIcon(image.getImage().getScaledInstance(Width, Height, Image.SCALE_AREA_AVERAGING)));
+        else
+            temp_label = new JLabel(new ImageIcon(image.getImage().getScaledInstance(25, 25, Image.SCALE_AREA_AVERAGING)));
+        if (Width != -1 && Height != -1)
+            temp_label.setBounds(x, y, Width, Height);
+        else
+            temp_label.setBounds(x, y, image.getIconWidth(), image.getIconHeight());
+        return temp_label;
+    }
+
+
+    static void Clear_Frame(JFrame Current_Frame) {
+        Current_Frame.getContentPane().removeAll();
+        Current_Frame.repaint();
+    }
+}
+
+class RoundedButton extends JButton {
+
+    private int radius;
+
+    public RoundedButton(int radius, String text) {
+        super(text);
+        this.radius = radius;
+        setContentAreaFilled(false); // Required when overriding paintComponent
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Call parent's paintComponent for basic functionality
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setColor(getBackground()); // Use button's background color
+        g2d.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
+
+        // Draw the text with some adjustment based on insets
+        int textX = (getWidth() - getFontMetrics(getFont()).stringWidth(getText())) / 2;
+        int textY = (getHeight() + getFontMetrics(getFont()).getAscent()) / 2;
+        g2d.setColor(getForeground()); // Use button's foreground color for text
+        g2d.drawString(getText(), textX, textY);
+    }
+}
+public class GUI_WORKER extends Front_End_Element_Code {
+    public static JFrame Main_Frame;
+    public static JButton LoginButton, RegisterButton;
+    public ArrayList<String> BookList = new ArrayList<String>(100);
+
+    public static void Frame(String Title) {
+        Main_Frame = Create_Frame(Title, PathDestinationToAssetsFolder + "local_library.png",800, 500);
+    }
+
+    // this is the welcome screen
+    public static void Panel_Number_1() {
+        Clear_Frame(Main_Frame);
+
+        JPanel Panel1 = Create_Panel(0, 0, Main_Frame.getWidth(), 100);
+
+        LoginButton = Create_Button(Main_Frame.getWidth() - 220, 5, "Login", 75, 50, 40);
+
+        RegisterButton = Create_Button(Main_Frame.getWidth() - 130, 5, "Register", 100, 50, 40);
+
+
+        LoginButton.setRolloverEnabled(false);
+        RegisterButton.setRolloverEnabled(false);
+
+        Panel1.add(LoginButton);
+        Panel1.add(RegisterButton);
+
+        JPanel Panel2 = Create_Panel(0, 100, Main_Frame.getWidth(), Main_Frame.getHeight() - 100);
+
+        final JLabel[] label1 = {Create_Label((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2) - 200, "Book,", 400, 100)};
+        label1[0].setFont(new java.awt.Font("League Spartan", Font.BOLD, 75));
+        label1[0].setForeground(new Color(0,0,0));
+
+        final JLabel[] label2 = {Create_Label((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2) - 125, "fast", 400, 100)};
+        label2[0].setFont(new java.awt.Font("League Spartan", Font.BOLD, 75));
+        label2[0].setForeground(new Color(0,0,0));
+
+        final JLabel[] label3 = {Create_Label((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2) - 50, "& read.", 400, 100)};
+        label3[0].setFont(new java.awt.Font("League Spartan", Font.BOLD, 75));
+        label3[0].setForeground(new Color(0,0,0));
+
+        final JLabel[] label4 = {Create_Label((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2), "Local Library", 400, 100)};
+        label4[0].setFont(new java.awt.Font("Merriweather", Font.PLAIN, 15));
+        label4[0].setForeground(new Color(0,0,0));
+
+        Panel2.add(label1[0]);
+        Panel2.add(label2[0]);
+        Panel2.add(label3[0]);
+        Panel2.add(label4[0]);
+
+        Main_Frame.add(Panel1);
+        Main_Frame.add(Panel2);
+
+        Main_Frame.repaint();
+        Main_Frame.revalidate();
+
+        Main_Frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+
+                Panel1.setBounds(0, 0, Main_Frame.getWidth(), 100);
+                LoginButton.setBounds(Main_Frame.getWidth() - 220, 5,75, 50);
+                RegisterButton.setBounds(Main_Frame.getWidth() - 130, 5, 100, 50);
+                Panel2.setBounds(0, 100, Main_Frame.getWidth(), Main_Frame.getHeight() - 100);
+                label1[0].setBounds((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2)  - 200, 400, 100);
+                label2[0].setBounds((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2)  - 125, 400, 100);
+                label3[0].setBounds((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2)  - 50, 400, 100);
+                label4[0].setBounds((Panel2.getWidth() / 2) - 150, (Panel2.getHeight() / 2), 400, 100);
+                Main_Frame.repaint();
+            }
+        });
+    }
+
+
+    // this is the login page, make sure to add the components in it
+    public static void Panel_Number_2() {
+        Clear_Frame(Main_Frame);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,283 @@
+import FrontEndSide.GUI_WORKER;
+
+import java.util.ArrayList;
+
+import static FrontEndSide.GUI_WORKER.*;
+
+
+public class Main {
+    static class Book {
+        private String BookName;
+        private String Author;
+        private ArrayList<Integer> BookRatingList;
+        private String BookDescription;
+        private int AmountOfViews;
+        private String Date_Taken;
+        private String Return_Due_Date;
+        private String Last_Date_Taken;
+        private ArrayList<String> ReadBy;
+        private Boolean Currently_Taken;
+
+        public Book(String BookName, String Author, String BookDescription){
+            this.BookName = BookName;
+            this.Author = Author;
+            this.BookDescription = BookDescription;
+            this.BookRatingList = new ArrayList<>();
+            this.ReadBy = new ArrayList<>();
+        }
+
+        public String getBookName() {
+            return BookName;
+        }
+        public String getAuthor() {
+            return Author;
+        }
+
+        public float getBookRating() {
+            int SumOfBookRatingList = 0;
+            for (int Rating:BookRatingList) {
+                SumOfBookRatingList += Rating;
+            }
+            return ((float) (SumOfBookRatingList) / (BookRatingList.size()));
+        }
+        public void addBookRating(int BookRating) {
+            this.BookRatingList.add(BookRating);
+        }
+
+        public String getBookDescription() {
+            return BookDescription;
+        }
+
+        public int getAmountOfViews() {
+            return AmountOfViews;
+        }
+        public void incrementAmountOfViews() {
+            this.AmountOfViews += 1;
+        }
+
+        public String getDate_Taken() {
+            return Date_Taken;
+        }
+        public void setDate_Taken(String Date_Taken) {
+            this.Date_Taken = Date_Taken;
+        }
+
+        public String getReturn_Due_Date() {
+            return Return_Due_Date;
+        }
+        public void setReturn_Due_Date(String Return_Due_Date) {
+            this.Return_Due_Date = Return_Due_Date;
+        }
+
+        public String getLast_Date_Taken() {
+            return Last_Date_Taken;
+        }
+        public void setLast_Date_Taken(String Last_Date_Taken) {
+            this.Last_Date_Taken = Last_Date_Taken;
+        }
+
+        public ArrayList<String> getAllReadBy() {
+            return ReadBy;
+        }
+        public String getLastReadBy_ByIndex(int Index) {
+            return ReadBy.get(Index);
+        }
+        public String getLastReadBy() {
+            return ReadBy.getLast();
+        } // ReadBy.get(LastReadBy.size() - 1)
+        public void addToReadBy(String UserName) {
+            this.ReadBy.add(UserName);
+        }
+
+        public Boolean getCurrently_Taken() {
+            return Currently_Taken;
+        }
+        public void setCurrently_Taken(Boolean Currently_Taken) {
+            this.Currently_Taken = Currently_Taken;
+        }
+    }
+    static class BookShelf {
+        private ArrayList<Book> BookShelfItems;
+
+        public BookShelf() {
+            this.BookShelfItems = new ArrayList<>();
+        }
+
+        public Book getBookByIndex(int Index) {
+            return BookShelfItems.get(Index - 1);
+        }
+
+        public ArrayList<Book> getAllBooks() {
+            return this.BookShelfItems;
+        }
+
+        public void addBook(Book book) {
+            this.BookShelfItems.add(book);
+        }
+
+        public void removeBook(int Index) {
+            this.BookShelfItems.remove(Index);
+        }
+
+        public void removeAllBooks() {
+            this.BookShelfItems.removeAll(BookShelfItems);
+        }
+    }
+
+    static class User {
+        private String UserName;
+        private String type;
+        private ArrayList<Book> ReadBooks;
+        public ArrayList<Book> BooksOnHand;
+        public ArrayList<Book> OverDueBooksOnHand;
+
+        public User(String UserName, String type) {
+            this.UserName = UserName;
+            this.type = type;
+            this.ReadBooks = new ArrayList<Book>();
+            this.BooksOnHand = new ArrayList<>();
+            this.OverDueBooksOnHand = new ArrayList<>();
+        }
+
+        public String getUserName() {
+            return UserName;
+        }
+
+        public ArrayList<Book> getReadBooks() {
+            return this.ReadBooks;
+        }
+
+        public void addToReadBooks(Book book) {
+            this.ReadBooks.add(book);
+        }
+
+        public ArrayList<Book> getBooksOnHand() {
+            return this.BooksOnHand;
+        }
+        public void addToBooksOnHand(Book book) {
+            if (this.BooksOnHand.size() <= 3) {
+                this.BooksOnHand.add(book);
+            } else {
+                System.out.println("To many book on hand!");
+            }
+        }
+        public void removeBookFromBooksOnHand_ByIndex(int Index) {
+            this.BooksOnHand.remove(Index);
+        }
+
+        public void removeAllBookFromBooksOnHand() {
+            this.BooksOnHand.removeAll(BooksOnHand);
+        }
+
+        public ArrayList<Book> getOverDueBooksOnHand() {
+            return this.OverDueBooksOnHand;
+        }
+        public void addToOverDueBooksOnHand(Book book) {
+            this.OverDueBooksOnHand.add(book);
+        }
+        public void removeOverDueBooksOnHand_ByIndex(int Index) {
+            this.OverDueBooksOnHand.remove(Index);
+        }
+
+        public void removeAllOverDueBooksOnHand() {
+            this.OverDueBooksOnHand.removeAll(OverDueBooksOnHand);
+        }
+    } // need to make search function
+
+    static class UsersList {
+        private ArrayList<User> Users;
+        public UsersList() {
+            this.Users = new ArrayList<>();
+        }
+
+        public void addUser(User user) {
+            this.Users.add(user);
+        }
+
+        public void removeUser_ByIndex(int Index) {
+            this.Users.remove(Index);
+        }
+        public void removeAllUser() {
+            this.Users.removeAll(Users);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BookShelf bs = new BookShelf();
+        Book n = new Book("New Book 0", "Jacob", "This book is tooo good");
+        n.addBookRating(10);
+        n.addBookRating(10);
+        n.addBookRating(10);
+        n.addBookRating(10);
+        n.addBookRating(9);
+        n.addBookRating(10);
+
+
+        bs.addBook(n);
+
+
+        n = new Book("New Book 1", "Lilly", "This book is mid");
+        n.addBookRating(1);
+        n.addBookRating(2);
+        n.addBookRating(2);
+        n.addBookRating(3);
+        n.addBookRating(9);
+        n.addBookRating(10);
+
+
+        bs.addBook(n);
+        n = new Book("New Book 2", "Robin", "This book is not good");
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(3);
+
+
+        bs.addBook(n);
+        n = new Book("New Book 3", "Des", "This book is not great");
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(10);
+        n.addBookRating(10);
+
+
+        bs.addBook(n);
+        n = new Book("New Book 4", "Nuzs", "This book is not bad");
+        n.addBookRating(10);
+        n.addBookRating(10);
+        n.addBookRating(3);
+        n.addBookRating(3);
+        n.addBookRating(7);
+        n.addBookRating(3);
+        n.addBookRating(10);
+        n.addBookRating(10);
+
+
+        bs.addBook(n);
+
+
+
+
+        for (Book book: bs.getAllBooks()) {
+            System.out.println("Book: " + book.getBookName() + "\nBook Description: " + book.getBookDescription() + "\nAuthor: " + book.getAuthor() + "\nRating: " + Float.toString(book.getBookRating()) + "\n");
+        }
+
+        Frame("Local Library"); // Makes a frame with the Title "Local Library"
+        Panel_Number_1(); // This will call the GUI function in "FrontEndSide/GUI_WORKER.java"
+
+
+        GUI_WORKER.LoginButton.addActionListener(v -> { // this access the global jbutton variable and checks if you did anything with the button like clicking
+            Panel_Number_2(); // this will show login page
+        });
+
+        GUI_WORKER.RegisterButton.addActionListener(v -> {
+            System.out.println("Register Please");
+        });
+    }
+}
+
+
